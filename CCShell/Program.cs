ee34968b8d6dfbae2d11c14563c5e88c1f6f5ad8
TimChen <timchen@live.com>
2023-09-04 21:35:53 +0800

    初始化项目

---
+++ CCShell/Program.cs
@@ -0,0 +1,33 @@
+using CCShell;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddServerComponents().AddWebAssemblyComponents();
+builder.Services.AddRazorPages();
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapRazorComponents<App>()
+    .AddServerRenderMode().AddWebAssemblyRenderMode();
+
+app.Run();
---
+++ CCShell/Shared/PageConfigAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace CCShell.Shared
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PageConfigAttribute: Attribute
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
